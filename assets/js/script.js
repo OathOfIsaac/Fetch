08dfb1d96fb249ab5ce01748282af7b316a133e4
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-03-19 15:59:41 -0500

    added temporary html and css to main page, working prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,30 @@ var factPrint = document.getElementById('fact-print')
 
 factButton.addEventListener('click' , getFact )
 
-var api = 'http://dog-api.kinduff.com';
+var api = 'https://dog-api.kinduff.com/api/facts';
+var parameter = '?number=1'
+
+// fetch(apiUrl)
+// .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     renderItems(city, data);
+//   })
+//   .catch(function (err) {
+//     console.error(err);
+//   });
+// }
 
 
 
 function getFact() {
     var url = api + parameter; 
-    console.log(url)
+    console.log(url);
+    console.log('hello');
+    
 }
 
 function gotData(data) {
     dogFact = data;
 }
-
-console.log
